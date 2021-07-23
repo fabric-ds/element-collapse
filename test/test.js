e736384439d68ca3644458d3649cc2f629240610
Dave Honneffer <pearofducks@gmail.com>
2021-07-23 10:09:06 +0200

    Whoops forgot to boot the server

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ import { init } from './serve.js'
 let exitCode
 const server = await init()
 try {
+  await server.listen(8080)
   const results = await cypress.run()
   exitCode = results.totalFailed === 0 ? 0 : 1
 } catch (err) {
