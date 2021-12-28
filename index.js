7e87f13c5b035b53b4b6ed269a653e361b17e364
Dave Honneffer <pearofducks@gmail.com>
2021-12-28 10:44:09 +0100

    Don't crash old browsers

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ let prefersMotion = true
 if (windowExists) {
   const query = window.matchMedia('(prefers-reduced-motion: reduce)')
   const callback = ({ matches }) => prefersMotion = !matches
-  query.addEventListener('change', callback)
+  // older browsers don't support the new API, and the old API is deprecated
+  if (query.addEventListener) query.addEventListener('change', callback)
   callback(query)
 }
 
